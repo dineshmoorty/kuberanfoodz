a73ad2a7b997753ce822a5e6c9fd4f1230b1b2eb
A S DineshMoorthi <dineshmoorthias@gmail.com>
2026-03-20 21:14:01 +0530

    Delete app/Views/admin directory

--- app/Views/admin/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?= view('/partials/adminheader') ?>
-<?= view('/partials/adminsidebar') ?>
-
-<div class="container">
-  <h1 class="mb-4">Admin Dashboard</h1>
-    
-</div>
-
-<?= view('/partials/adminfooter') ?>
